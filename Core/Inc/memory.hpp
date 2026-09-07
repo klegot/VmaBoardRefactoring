dfeb8d993eee691fc1268a2a244b196558d75e49
klegot <redwolf.2007@mail.ru>
2026-09-07 18:23:11 +0000

    комменты для красоты

--- Core/Inc/memory.hpp
+++ Core/Inc/memory.hpp
@@ -1,11 +1,11 @@
 #include "main.h"
 
 struct MemoryMap {
-  uint8_t pid_target_speed_rpm_conversion[MOTORS_COUNT];
-  uint8_t pwm_targets_conversion[4];
-  uint16_t vbat1_adc;
-  uint16_t vbat2_adc;
-  bool killswitch_state;
+  uint8_t pid_target_speed_rpm_conversion[MOTORS_COUNT]; // address 0 length 10
+  uint8_t pwm_targets_conversion[4];                     // address 10 length 4
+  uint16_t vbat1_adc;                                    // address 14 length 2
+  uint16_t vbat2_adc;                                    // address 16 length 2
+  bool killswitch_state;                                 // address 18 length 1
 } __attribute__((__packed__));
 
 class Memory {
